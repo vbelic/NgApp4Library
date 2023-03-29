338cf8fdd8c968eb51dd44d0a60a0ef4eba178c1
vbelic <vbelic@yahoo.com>
2023-03-29 03:23:30 +0200

    Ovveride library component

--- App4LibraryImplementation/src/app/app.module.ts
+++ App4LibraryImplementation/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyLbModule } from 'my-lb';
+import { OvverideComponent } from './ovveride.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OvverideComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     MyLbModule
   ],
